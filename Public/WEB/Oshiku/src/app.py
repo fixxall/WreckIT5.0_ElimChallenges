220d3925810f1edd75d00307e65bfebd98056e1d
VascoZ <86867624+VascoZ@users.noreply.github.com>
2024-07-28 19:27:24 +0700

    Update app.py

--- Public/WEB/Oshiku/src/app.py
+++ Public/WEB/Oshiku/src/app.py
@@ -23,7 +23,7 @@ def query_database(name):
 def index():
     role = session.get('role')
     if role == "admin":
-        return redirect(url_for('admin'))
+        return redirect(url_for('index.html'))
     elif role == "guest":
         return redirect(url_for('guest'))
     else:
