aa45ea6172b02c663ca00d9433661450b3b7c364
Rayhan Hanaputra <hanaputrarayhan@gmail.com>
2024-07-28 12:58:58 +0700

    slim

--- Public/PWN/slim/src/start.sh
+++ Public/PWN/slim/src/start.sh
@@ -4,7 +4,7 @@
 service ssh start
 
 # Setup cron job to copy flag.txt every 30 seconds
-echo "*/1 * * * * root cp /flag.txt /destination/flag.txt" > /etc/cron.d/copy-flag
+echo "*/1 * * * * root cp /var/tmp/flag.txt /flag.txt" > /etc/cron.d/copy-flag
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/copy-flag
